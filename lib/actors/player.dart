22ca65c11426c90e13f808408272d610a99968cc
sawanrmodi <sawan.modi@quickeselling.com>
2024-07-08 12:58:13 +0530

    Your commit message here

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,16 +1,25 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
 import 'package:game/pixel_adventure.dart';
 
-enum PlayerState {idle,running}
+enum PlayerState { idle, running }
+
+enum PlayerDirection { left, right, none }
+
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
-  Player({required this.character,position}) : super(position: position);
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
+  Player({this.character = 'Ninja Frog', position}) : super(position: position);
   String character;
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
+
+  PlayerDirection playerDirection = PlayerDirection.none;
+  double movespeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
 
   @override
   FutureOr<void> onLoad() {
@@ -18,20 +27,46 @@ class Player extends SpriteAnimationGroupComponent
     return super.onLoad();
   }
 
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    if (isRightKeyPressed && isLeftKeyPressed) {
+      playerDirection = PlayerDirection.none;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else{
+      playerDirection = PlayerDirection.none;
+    }
+    // TODO: implement onKeyEvent
+    return super.onKeyEvent(event, keysPressed);
+  }
+
   void _loadAllAnimations() {
     idleAnimation = _spriteAnimation('Idle', 11);
-
-    runningAnimation = _spriteAnimation('Run',12);
+    runningAnimation = _spriteAnimation('Run', 12);
 
     //Lis of all animations
     animations = {
-      PlayerState.idle : idleAnimation,
-      PlayerState.running : runningAnimation
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation
     };
     //set current animation
     current = PlayerState.idle;
   }
-  SpriteAnimation _spriteAnimation(String state,int amount){
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
     return SpriteAnimation.fromFrameData(
         game.images.fromCache('Main Characters/$character/$state (32x32).png'),
         SpriteAnimationData.sequenced(
@@ -39,5 +74,34 @@ class Player extends SpriteAnimationGroupComponent
           stepTime: stepTime,
           textureSize: Vector2.all(32),
         ));
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running;
+        dirX -= movespeed;
+        break;
+      case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running;
+        dirX += movespeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle;
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity * dt;
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,27 +1,70 @@
 import 'dart:async';
-import 'dart:ui';
 
-import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:game/levels/level.dart';
 
-class PixelAdventure extends FlameGame{
-  @override
-  Color backgroundColor()=> const Color(0xFF211F30);
+import 'actors/player.dart';
 
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
+  Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
   @override
-  final world = Level(
-  levelName: 'level-02');
-  @override
-  FutureOr<void> onLoad() async{
-
+  FutureOr<void> onLoad() async {
     await images.loadAllImages();
-
-    cam = CameraComponent.withFixedResolution(width: 640, height: 360,world:world );
+    final world = Level(levelName: 'level-02', player: player);
+    cam = CameraComponent.withFixedResolution(
+        width: 650, height: 315, world: world);
     cam.viewfinder.anchor = Anchor.topLeft;
-addAll([cam,world]);
-return super.onLoad();
+    addAll([cam, world]);
+    addJoystick();
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    updateJoystick();
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+          sprite: Sprite(
+        images.fromCache('HUD/Joystick.png'),
+      )),
+      margin: const EdgeInsets.only(left: 32,bottom: 2),
+    );
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.downLeft:
+      case JoystickDirection.upLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.downRight:
+      case JoystickDirection.upRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+
+        break;
+    }
   }
 }
